e22686133868e02cc9af98d1d8ef41f522a04ec5
Jose Alonzo <pepemxl@gmail.com>
2026-05-01 13:07:42 -0600

    initialize module

---
+++ pplogger/__init__.py
@@ -0,0 +1,6 @@
+"""pplogger - common logger for observability of distributed systems."""
+
+from pplogger.logger import build_log_path, initializer_logger
+
+__all__ = ["build_log_path", "initializer_logger"]
+__version__ = "0.0.1"
